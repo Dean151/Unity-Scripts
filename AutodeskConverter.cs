fe5648ffdf6d4a1cfb47ff6e172742461813af82
Thomas Durand <Dean151@users.noreply.github.com>
2015-04-10 16:37:51 -0400

    Create AutodeskConverter.cs

---
+++ AutodeskConverter.cs
@@ -0,0 +1,52 @@
+/**
+  * AutodeskConverter.cs
+  * Allow to convert vectors between Autodesk and Unity standards
+  * Created by Thomas Durand
+  * 2015-04-10
+  */
+
+using UnityEngine;
+using System;
+using System.Collections;
+
+public static class AutoDeskConverter {
+
+	/**
+	  * Convert a string like "x|y|z" to a Vector3
+	  */
+	public static Vector3 StringToVector3(string input)
+	{
+		string[] coords = input.Split("|"); 
+		if (coords.Length == 3) 
+		{
+			return new Vector3( 
+					float.parse(coords[0]),
+					float.parse(coords[1]),
+					float.parse(coords[2])
+				);
+		}
+		else
+		{
+			Debug.LogError(input + "could not be casted to Vector3");
+			return Vector3.Zero;
+		}
+	}
+	
+	/**
+	  * Convert a Vector3 from Autodesk Standard to Unity Standard
+	  */
+	public static Vector3 AutodeskToUnity(Vector3 input) 
+	{
+		// Y and Z axis are inversed in Unity
+		return new Vector3( input.x , input.z , input.y );
+	}
+
+	/**
+	  * Convert a Vector3 from Unity Standard to Autodesk Standard
+	  */
+	public static Vector3 UnityToAutodesk(Vector3 input) 
+	{
+		// Actually this function is just an Alias to clarify Source Code
+		return AutodeskToUnity(input);
+	}
+}
